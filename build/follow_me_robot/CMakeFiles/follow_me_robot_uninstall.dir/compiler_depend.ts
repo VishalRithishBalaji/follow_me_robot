ea58a79a2b8c71bbc0d7b126c46cf7dc791e88ae
Your Name <your_email@example.com>
2026-01-22 19:03:26 +0530

    Cleanup: Ignore build, install, and log artifacts

--- build/follow_me_robot/CMakeFiles/follow_me_robot_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for follow_me_robot_uninstall.
--- build/follow_me_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vishal/ros2_ws/src/follow_me_robot
-# Build directory: /home/vishal/ros2_ws/src/follow_me_robot/build/follow_me_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
